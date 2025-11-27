4d527beb41e73b65cebdcb23ac0f559ca831ade7
Genji1811 <81412422+Genji1811@users.noreply.github.com>
2025-11-27 11:38:52 +0700

    Add files via upload

---
+++ incompleted/scanner.h
@@ -0,0 +1,19 @@
+/* Scanner
+ * @copyright (c) 2008, Hedspi, Hanoi University of Technology
+ * @author Huu-Duc Nguyen
+ * @version 1.0
+ */
+
+#ifndef __SCANNER_H__
+#define __SCANNER_H__
+
+#include "token.h"
+
+void skipBlank(void);
+int scanFILE(char *fileName);
+
+Token* getToken(void);
+Token* getValidToken(void);
+void printToken(Token *token);
+
+#endif
